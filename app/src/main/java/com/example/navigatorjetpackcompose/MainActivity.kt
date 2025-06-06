dd6d220075ce6f9687a7b6d3b960045af2daa6d1
redondoJulian <juliantw3012@gmail.com>
2025-06-06 16:37:52 -0300

    adding navigator

---
+++ app/src/main/java/com/example/navigatorjetpackcompose/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.navigatorjetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.navigatorjetpackcompose.screens.FirstPage
+import com.example.navigatorjetpackcompose.screens.SecondPage
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            val navController = rememberNavController()
+            var contador by remember { mutableIntStateOf(0) }
+            Scaffold(
+                floatingActionButton = {
+                    FloatingActionButton(
+                        onClick = { contador++ },
+                        containerColor = Color.Black,
+                        contentColor = Color.White,
+                        shape = CircleShape
+                    ) {
+                        Icon(contentDescription = "Add", imageVector = Icons.Default.AddCircle)
+                    }
+                }
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                ) {
+                    NavHost(navController = navController, startDestination = "first") {
+                        composable(route = "first") { FirstPage(navController, contador) }
+                        composable(route = "second") { SecondPage(navController, contador) }
+                    }
+                }
+
+            }
+        }
+    }
+}
